3eae1b550b1ad8d40a88e08afb0644763fb0663c
wormsJJG <jjr66461837@gmail.com>
2022-10-30 01:08:45 +0900

    fix: 주석 제거, 파일정리

--- Solution/Solution/Controller/MainViewController.swift
+++ Solution/Solution/Controller/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Solution
 //
 //  Created by 정재근 on 2022/10/30.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
-
---
+++ Solution/Solution/Delegate/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  Solution
+//
+//  Created by 정재근 on 2022/10/30.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
