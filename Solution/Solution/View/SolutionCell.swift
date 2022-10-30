5e08a3a27f89c13d2ad5069c43b2ccd92813f250
wormsJJG <jjr66461837@gmail.com>
2022-10-31 00:21:58 +0900

    feat: SolutionCell-titlelabel 레이아웃

--- Solution/Solution/View/SolutionCell.swift
+++ Solution/Solution/View/SolutionCell.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
+import Then
 
 class SolutionCell: UICollectionViewCell {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel().then {
+            $0.textColor = .white
+            $0.font = UIFont.boldSystemFont(ofSize: 19)
+            $0.textAlignment = .left
+            $0.numberOfLines = 1
+            $0.text = "제목"
+        }
+        
+        return label
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         setCellLayer()
+        configureTitleLabel()
     }
 
     required init?(coder: NSCoder) {
@@ -25,5 +39,15 @@ class SolutionCell: UICollectionViewCell {
         self.layer.shadowOpacity = 0.2
         self.layer.shadowRadius = 1.0
         self.backgroundColor = #colorLiteral(red: 0.4613699913, green: 0.3118675947, blue: 0.8906354308, alpha: 1)
+    }
+    
+    private func configureTitleLabel() {
+        self.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { label in
+            label.left.equalTo(self.safeAreaLayoutGuide.snp.left).offset(15)
+            label.right.equalTo(self.safeAreaLayoutGuide.snp.right).offset(-15)
+            label.top.equalTo(self.safeAreaLayoutGuide.snp.top).offset(60)
+//            label.centerY.equalToSuperview()
+        }
     }
 }
